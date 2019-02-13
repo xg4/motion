bfa37bea7b6c3139bd37288547152ee1db0e4de7
xg4 <xingor4@gmail.com>
2019-02-13 19:53:57 +0800

    chore: add editor config, es module

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,32 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
-import { uglify } from 'rollup-plugin-uglify'
+import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 export default {
   input: 'src/index.js',
   output: [
     {
+      name: 'XMotion',
       file: pkg.main,
       format: 'umd',
-      name: 'Motion',
       globals: {
-        raf: 'raf',
-        easings: 'easings'
+        raf: 'raf'
       }
+    },
+    {
+      file: pkg.module,
+      format: 'es'
     }
   ],
-  external: ['raf', 'easings'],
+  external: ['raf'],
   plugins: [
+    resolve(),
+    commonjs(),
     babel({
       exclude: 'node_modules/**'
     }),
-    uglify()
+    terser()
   ]
 }
