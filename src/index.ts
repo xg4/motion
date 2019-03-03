5be25988953b16e6b30d9f221c9a8058f91da848
xg4 <xingor4@gmail.com>
2019-03-04 00:26:42 +0800

    refactor: for better chore: add tslint

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,56 @@
-import raf from 'raf'
 import { easeInOutCubic } from '@xg4/easings'
+import raf from 'raf'
+
+type easeFunc = (t: number, b: number, c: number, d: number) => number
 
 export interface MotiveOptions {
   from?: number
   to?: number
   duration?: number
-  timingFunc?: Function
-  count?: number // 0:'infinite', 1:'stop', >1:'count'
+  ease?: easeFunc
+  count?: number // Infinity, 1:'stop', >1:'count'
   reverse?: boolean
 }
 
-export function move(callback: Function, options: MotiveOptions = {}) {
-  return new Promise(resolve => {
+export function move(
+  callback: (currentValue?: number) => void,
+  options: MotiveOptions = {},
+) {
+  return new Promise((resolve) => {
     const {
       from = 0,
       to = 0,
       duration = 450,
-      timingFunc = easeInOutCubic,
+      ease = easeInOutCubic,
       count = 1,
-      reverse
+      reverse,
     } = options
     const startTime = Date.now()
     const frameFunc = () => {
       const timestamp = Date.now()
       const time = timestamp - startTime
 
-      callback(timingFunc(time, from, to, duration))
+      callback(ease(time, from, to, duration))
       if (time < duration) {
         raf(frameFunc)
       } else {
+        // over
         callback(to)
+
+        const parseCount = Number(count)
+        if (parseCount === 1 || Number.isNaN(parseCount)) {
+          resolve()
+          return
+        }
 
         if (reverse) {
           options.to = from
           options.from = to
         }
-
-        if (count === 1) {
-          resolve()
-        } else {
-          if (count > 1) {
-            options.count = count - 1
-          }
-
-          move(callback, options)
+        if (count > 1) {
+          options.count = count - 1
         }
+        move(callback, options)
       }
     }
     raf(frameFunc)
